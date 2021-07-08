209b2d63c6880c3e674590e683e2b3e9e8c79214
AlonFlor <43430118+AlonFlor@users.noreply.github.com>
2021-07-07 20:36:18 -0400

    Add files via upload
    Added disease modelling

--- MIGBasicSim.cpp
+++ MIGBasicSim.cpp
@@ -7,39 +7,51 @@
 
 using namespace std;
 
-float interaction_strength = 1.0;
+float interaction_strength = 5.0;
 float interaction_range = 1.0;
 float relaxation_time = 1.0;
 float desired_speed = 1.0;
 
-float interaction_radius = 0.5;
+float interaction_radius = 1.5;
 //float max_interation_force = 20;
 
 const float dt = 0.001;
 const float total_time = 100;
-const int num_people = 50;
+const int num_people = 100;
 
 
-Person initialize(float initial_X[], float initial_V[], float desired_displacement[])
+Person initialize(float initial_X[], float initial_V[], float desired_displacement[], int group_ID)
 {
 	//Places
 	float destination[2];
 	destination[0] = initial_X[0] + desired_displacement[0];
 	destination[1] = initial_X[1] + desired_displacement[1];
-	return Person(initial_X, initial_V, destination, desired_speed);
+	return Person(initial_X, initial_V, destination, desired_speed, group_ID);
 }
 
 
-float* interaction_force(Person p1, Person p2)
+float** interaction_force_and_disease_spread(Person p1, Person p2)
 {
-	float* ans = diff(p1.X, p2.X);
-	float distance = dist(ans);
+	float* difference = diff(p2.X, p1.X);
+	float distance = dist(difference);
 	float mult_factor = interaction_strength * exp(-1 * distance / interaction_range);
+	float disease_mult_factor = 1.0;
 	if (distance > interaction_radius) {
 		mult_factor = 0.0;
+		disease_mult_factor = 0.0;
 	}
-	ans[0] = mult_factor * ans[0] / distance;
-	ans[1] = mult_factor * ans[1] / distance;
+	float force_ans[2];
+	force_ans[0] = mult_factor * difference[0] / distance;
+	force_ans[1] = mult_factor * difference[1] / distance;
+
+	float disease_ans[1];
+	float disease_difference = max(p2.disease - p1.disease, 0.0f);	//if p1 is more more infected than p2, p2 cannot infect p1.
+	float distance_sq = distance * distance;
+	disease_ans[0] = exp(-1*distance) * disease_difference * disease_mult_factor / distance_sq;
+
+	float* ans[2];
+	ans[0] = force_ans;
+	ans[1] = disease_ans;
 
 	/*//cap force
 	float amount = dist(ans);
@@ -50,37 +62,49 @@ float* interaction_force(Person p1, Person p2)
 	return ans;
 }
 
-float* net_interaction_force(int p1_index, Person people[])
+float** net_interaction_force_and_disease_spread(int p1_index, Person people[])
 {
 	float force[2];
 	force[0] = 0.0;
 	force[1] = 0.0;
+	float disease_change[1];
+	disease_change[0] = 0.0;
+
 	for (int i = 0; i < num_people; ++i) {
 		if (i != p1_index) {
-			float* this_force = interaction_force(people[i], people[p1_index]);
+			float** this_force_and_disease_spread = interaction_force_and_disease_spread(people[p1_index], people[i]);
+
+			float* this_force = this_force_and_disease_spread[0];
 			force[0] += this_force[0];
 			force[1] += this_force[1];
+
+			float* disease_spread = this_force_and_disease_spread[1];
+			disease_change[0] += disease_spread[0];
 		}
 	}
-	return force;
+
+	float* ans[2];
+	ans[0] = force;
+	ans[1] = disease_change;
+	return ans;
 }
 
 int main()
 {
 	//centers
 	float initial_center1[2];
-	initial_center1[0] = 1.0;
-	initial_center1[1] = 1.0;
+	initial_center1[0] = -15.0;
+	initial_center1[1] = 0.0;
 	float initial_center2[2];
-	initial_center2[0] = 4.0;
-	initial_center2[1] = 0.5;
+	initial_center2[0] = 15.0;
+	initial_center2[1] = 0.0;
 
 	//displacements
 	float displacement1[2];
-	displacement1[0] = 1.0;
+	displacement1[0] = 30.0;
 	displacement1[1] = 0.0;
 	float displacement2[2];
-	displacement2[0] = -8.0;
+	displacement2[0] = -30.0;
 	displacement2[1] = 0.0;
 
 	//initial velocity
@@ -92,24 +116,27 @@ int main()
 	Person people[num_people];
 	int count = 0;
 	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
+		for (int j = 0; j < 10; ++j) {
 			float x[2];
 			x[0] = initial_center1[0] - j;
 			x[1] = initial_center1[1] + i;
-			people[count] = initialize(x, initial_V, displacement1);
+			people[count] = initialize(x, initial_V, displacement1,0);
 			count += 1;
 		}
 	}
 	count = 0;
 	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
+		for (int j = 0; j < 10; ++j) {
 			float x[2];
 			x[0] = initial_center2[0] + j;
 			x[1] = initial_center2[1] + i;
-			people[count + 25] = initialize(x, initial_V, displacement2);
+			people[count + 50] = initialize(x, initial_V, displacement2,1);
 			count += 1;
 		}
 	}
+
+	people[42].disease = 1.0; //one person is infected
+	
 	//people[0] = initialize(initial_center1, initial_V, displacement1);
 	//people[1] = initialize(initial_center2, initial_V, displacement2);
 
@@ -117,11 +144,14 @@ int main()
 	
 	//main loop
 	FILE* fp;
+	int img_count = 1;
 	for (int i = 0; i < num_steps; ++i) {
 		if (i % 100 == 0) {
-			float current_time = (float(i)) * dt;
+			//float current_time = (float(i)) * dt;
 			char t[50];
-			sprintf(t, "%f.csv", current_time);
+			//sprintf(t, "%f.csv", current_time);
+			sprintf(t, "%04d.csv",img_count);
+			img_count += 1;
 
 			//open file
 			fp = fopen(t, "w");
@@ -131,11 +161,13 @@ int main()
 			fprintf(fp, ",pos_y");
 			fprintf(fp, ",v_x");
 			fprintf(fp, ",v_y");
+			fprintf(fp, ",group_ID");
+			fprintf(fp, ",disease");
 			fprintf(fp, "\n");
 		}
 
 		for (int j = 0; j < num_people; ++j) {
-			if(i%100==0)fprintf(fp, "%f,%f,%f,%f\n", people[j].X[0], people[j].X[1], people[j].V[0], people[j].V[1]); //print person position and velocity
+			if(i%100==0)fprintf(fp, "%f,%f,%f,%f,%d,%f\n", people[j].X[0], people[j].X[1], people[j].V[0], people[j].V[1], people[j].group_ID, people[j].disease); //print person position and velocity
 
 			float* desired_velocity = people[j].get_desired_velocity();
 
@@ -143,20 +175,24 @@ int main()
 			desired_velocity_force[0] = (desired_velocity[0] - people[j].V[0]) / relaxation_time;
 			desired_velocity_force[1] = (desired_velocity[1] - people[j].V[1]) / relaxation_time;
 
-			float* this_net_interaction_force = net_interaction_force(j, people);
+			float** force_and_disease = net_interaction_force_and_disease_spread(j, people);
+			float* this_net_interaction_force = force_and_disease[0];
+			float this_disease_change = force_and_disease[1][0];
 
 			float acceleration[2];
 			acceleration[0] = desired_velocity_force[0] +this_net_interaction_force[0];
 			acceleration[1] = desired_velocity_force[1] +this_net_interaction_force[1];
 
-			people[j].update(dt, acceleration);
+			people[j].update_pos_vel(dt, acceleration);
+			people[j].update_disease(dt, this_disease_change);
 		}
 		//close file
 		if(i%100==0)fclose(fp);
 	}
-	float current_time = (float(num_steps)) * dt;
+	//float current_time = (float(num_steps)) * dt;
 	char t[50];
-	sprintf(t, "%f.csv", current_time);
+	//sprintf(t, "%f.csv", current_time);
+	sprintf(t, "%04d.csv", img_count);
 
 	//open file
 	fp = fopen(t, "w");
@@ -166,12 +202,13 @@ int main()
 	fprintf(fp, ",pos_y");
 	fprintf(fp, ",v_x");
 	fprintf(fp, ",v_y");
+	fprintf(fp, ",group_ID");
+	fprintf(fp, ",disease");
 	fprintf(fp, "\n");
-	fprintf(fp, "%f", current_time);
 
 	//print people positions and velocities
 	for (int j = 0; j < num_people; ++j) {
-		fprintf(fp, "%f,%f,%f,%f\n", people[j].X[0], people[j].X[1], people[j].V[0], people[j].V[1]);
+		fprintf(fp, "%f,%f,%f,%f,%d,%f\n", people[j].X[0], people[j].X[1], people[j].V[0], people[j].V[1], people[j].group_ID, people[j].disease);
 	}
 	//close file
 	fclose(fp);
--- Person.h
+++ Person.h
@@ -9,13 +9,16 @@ public:
 	float V[dim];
 	float destination[dim];
 	float desired_speed;
+	float disease = 0.0;
+	float mask = 0.0;
+	int group_ID;
 
 	Person()
 	{
 		
 	}
 
-	Person(float* X_in, float* V_in, float* destination_in, float desired_speed_in)
+	Person(float* X_in, float* V_in, float* destination_in, float desired_speed_in, int group_ID_in)
 	{
 		X[0] = X_in[0];
 		X[1] = X_in[1];
@@ -24,6 +27,7 @@ public:
 		destination[0] = destination_in[0];
 		destination[1] = destination_in[1];
 		desired_speed = desired_speed_in;
+		group_ID = group_ID_in;
 	}
 
 	float* get_desired_velocity()
@@ -40,10 +44,17 @@ public:
 		return ans;
 	}
 
-	void update(float dt, float acceleration[]) {
+	void update_pos_vel(float dt, float acceleration[]) {
 		X[0] = X[0] + V[0] * dt;
 		X[1] = X[1] + V[1] * dt;
 		V[0] = V[0] + acceleration[0] * dt;
 		V[1] = V[1] + acceleration[1] * dt;
+	}
+
+	void update_disease(float dt, float disease_in) {
+		disease += disease_in * dt;
+		if (disease > 1.0) {
+			disease = 1.0;
+		}
 	}
 };
